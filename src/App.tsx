4b247e8d06bd382762360eb9b640c1caad755cce
WolfieLeader <koren2318@gmail.com>
2023-08-25 01:12:03 +0300

    First Step Towards Big Event

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import React, { lazy, Suspense } from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import "./styles/default.css";
+
+import Loading from "./pages/Loading";
+
+// * Import Here Any Page Component
+const Home = lazy(() => import("./pages/Home"));
+
+const App = () => {
+  return (
+    <div className="flex flex-col h-screen text-textColor bg-backgroundColor">
+      <Suspense fallback={<Loading />}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </Suspense>
+    </div>
+  );
+};
+
+export default App;
