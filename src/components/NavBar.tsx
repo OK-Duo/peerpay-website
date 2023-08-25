080dab898bd8287e9d169799a98de2c8f19bae54
WolfieLeader <koren2318@gmail.com>
2023-08-25 14:54:44 +0300

    Nav Bar Done

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,28 +3,53 @@ import Icon from "../assets/svg/Icon";
 import TelegramIcon from "../assets/svg/TelegramIcon";
 import TwitterIcon from "../assets/svg/TwitterIcon";
 import LinkedinIcon from "../assets/svg/LinkedinIcon";
+import { Link } from "react-router-dom";
+import GithubIcon from "../assets/svg/GithubIcon";
 
 const NavBar = () => {
+  const socialLinks = [
+    {
+      name: "Telegram",
+      link: "https://web.telegram.org",
+      icon: <TelegramIcon />,
+    },
+    {
+      name: "Twitter",
+      link: "https://twitter.com",
+      icon: <TwitterIcon />,
+    },
+    {
+      name: "Linkedin",
+      link: "https://linkedin.com",
+      icon: <LinkedinIcon />,
+    },
+    {
+      name: "Github",
+      link: "https://github.com",
+      icon: <GithubIcon />,
+    },
+  ];
   return (
-    <header className="w-full z-20 top-0 flex items-center py-4 px-2 sm:px-4 md:px-8">
+    <header className="w-full z-20 top-0 flex items-center py-4 px-4 sm:px-6 md:px-8">
       <div className="mx-auto container flex items-center justify-between">
         <div className="flex items-center justify-center">
-          <span className="w-10 h-10">
+          <span className="w-9 h-9 mr-[0.35rem]">
             <Icon />
           </span>
-          <span className="ml-2 text-primary font-bold text-xl hidden sm:block">Peerpay</span>
+          <h1 className="text-primary font-bold text-xl hidden sm:block">peerpay</h1>
         </div>
         <nav>
-          <ul className="flex items-center justify-around text-slate-900">
-            <li className="w-8 h-8 p-1 bg-slate-300 rounded-lg mx-1">
-              <TelegramIcon />
-            </li>
-            <li className="w-8 h-8 p-1 bg-slate-300 rounded-lg mx-1">
-              <TwitterIcon />
-            </li>
-            <li className="w-8 h-8 p-1 bg-slate-300 rounded-lg mx-1">
-              <LinkedinIcon />
-            </li>
+          <ul className="flex items-center justify-around text-slate-300">
+            {socialLinks.map((link) => {
+              return (
+                <li key={link.name} className="w-6 h-6 mx-1 sm:mx-1.5">
+                  <Link to={link.link} target="_blank">
+                    <span className="sr-only">{link.name}</span>
+                    {link.icon}
+                  </Link>
+                </li>
+              );
+            })}
           </ul>
         </nav>
       </div>
